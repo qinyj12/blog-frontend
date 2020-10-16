1b476c69d4d930719dffbae5421a9eadd6166e6f
qinyj12 <qinyj12@gmail.com>
2020-10-16 20:26:20 +0800

    下一步：做完文章推荐区块

--- src/views/Content.vue
+++ src/views/Content.vue
@@ -29,16 +29,37 @@
                     <div>{{AboutAuthor.introduction}}</div>
                     <ul>
                         <li v-for="item in AboutAuthor.contact" :key="item">
+                            <!-- 动态添加微博、微信、github等icon -->
                             <i :class="'fab fa-' + item"></i>
                         </li>
                     </ul>
                 </div>
             </div>
 
-            <div class="articles-recommended">
-                <div class="single-article-recommended"></div>
-                <div></div>
-            </div>
+            <ul class="articles-recommended">
+                <li class="single-article-recommended">
+                    <img src="../assets/featured-image.png" alt="cover" class="article-recommended-cover">
+                    <div class="article-recommended-introduction">
+                        <div class="article-recommended-tag-wrap">
+                            <div class="article-recommended-tag">案例</div>
+                        </div>
+                        <!-- 文字溢出添加省略号 -->
+                        <v-clamp 
+                            max-height="calc(100% - 84px)" 
+                            autoresize="true"
+                            class="article-recommended-title"
+                        >
+                            这是第一篇文章这是第一篇文章这是第一篇文章这是第一篇文章这是第一篇文章第一篇文章第一篇文章第一篇文章第一篇文章123
+                        </v-clamp>
+                        <div class="recommended-avatar-views">
+                            <div class="recommended-avatar"></div>
+                            <div class="recommended-author">测试用户</div>
+                            <i class="far fa-eye"></i>
+                            <div>50</div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
         </div>
 
         <Footer />
@@ -47,11 +68,13 @@
 </template>
 <script>
 import Cover from '@/components/Cover.vue';
-import Footer from '@/components/Footer.vue'
+import Footer from '@/components/Footer.vue';
+import VClamp from 'vue-clamp'
 export default {
     components: {
         Cover,
-        Footer
+        Footer,
+        VClamp
     },
     data() {
         return {
@@ -136,8 +159,81 @@ sink-time = 0.2s
             }
         }
 
-        .articles-recommended {
-            border 1px solid
+        ul.articles-recommended {
+            list-style none
+            padding 0
+
+            li.single-article-recommended {
+                height 200px
+                width 100%
+                display flex
+                border-radius 4px
+                overflow hidden
+                box-sizing border-box
+                box-shadow 0px 1px 4px 0px rgba(0, 0, 0, 0.15)
+
+                img.article-recommended-cover {
+                    width 50%
+                    height 100%
+                    object-fit cover
+                    box-sizing border-box
+                }
+
+                .article-recommended-introduction {
+                    height 100%
+                    width 50%
+                    box-sizing border-box
+                    text-align left 
+                    padding 30px
+
+                    display flex
+                    flex-direction column
+
+                    .article-recommended-tag-wrap {
+                        height 24px
+                        margin-bottom 10px
+
+                        .article-recommended-tag {
+                            display inline-block
+                            border-radius 12px
+                            padding 0 15px
+                            background-color #009CFF
+                            color white
+                            height 100%
+                        }
+                    }
+
+                    .article-recommended-title {
+                        font-weight bold
+                        font-size large
+                        flex 1
+                    }
+
+                    .recommended-avatar-views {
+                        display flex
+                        align-items center
+                        height 40px
+
+                        margin-top 10px
+
+                        * {
+                            margin-right 5px
+                        }
+
+                        .recommended-avatar {
+                            width 32px
+                            height 32px
+                            border-radius 50%
+                            background-image url('../assets/avatar.png')
+                            background-size cover
+                        }
+
+                        .recommended-author {
+                            margin-right 15px
+                        }
+                    }
+                }
+            }
         }
 
         .content-rise-enter-active {
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     publicPath: './',
-    filenameHashing: false
+    filenameHashing: false,
+    transpileDependencies: ['vue-clamp', 'resize-detector']
 }
